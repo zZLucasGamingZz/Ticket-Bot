53bb6cd5f125ccb1064606ef19c6d404edcc1d26
zZLucasGamingZz <80544421+zZLucasGamingZz@users.noreply.github.com>
2021-07-23 15:35:15 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+const { Client, MessageEmbed } = require('discord.js');
+const client = new Client();
+const config = require("./config.json");
+
+
+["Ready", "Message"].forEach(handler => {
+    require(`./src/Events/${handler}`)(client);
+});
+
+require('discord-buttons')(client);
+const { MessageButton, MessageActionRow } = require('discord-buttons')
+
+const embed = new MessageEmbed()
+.setTitle("Suporte")
+.setDescription('Clique em **📩 Fechar Ticket** para fechar o suporte')
+.setColor('GREEN')
+let botao = new MessageButton()
+.setStyle('gray')
+.setLabel('Fechar Ticket')
+.setEmoji('📩')
+.setID('ticketclose')
+const row = new MessageActionRow()
+.addComponent(botao)
+
+client.on("clickButton", async (button) => {
+    if(button.id == 'suporte') {
+        const member = button.clicker.member
+        const canal = member.guild.channels.cache.find(ch => ch.name === `${member.id}`);
+        if (canal) return member.guild.channel.send(`${member} Seu ticket atual já está aberto em: ${canal}!`).then(msg => msg.delete({timeout: 15000}));
+        member.guild.channels.create(`${member.id}`, {
+            type : 'text',
+            permissionOverwrites : [
+                {
+                    id : member.guild.id,
+                    deny : ['VIEW_CHANNEL']
+                },
+                {
+                    id : member.id,
+                    allow : ['VIEW_CHANNEL', 'SEND_MESSAGES', 'ATTACH_FILES']
+                }
+            ]
+        }).then(async channel => {
+            channel.send(`Olá ${member}, este é o seu ticket!`)
+            channel.send(embed, botao, row)
+        })
+    }})
+
+client.on("clickButton", async (button) =>{
+    if(button.id == 'ticketclose') {
+        const member = button.clicker.member
+        const channel = member.guild.channels.cache.find(ch => ch.name === `${member.id}`)
+        channel.delete()
+    }
+})
+
+
+client.login(config.token);
